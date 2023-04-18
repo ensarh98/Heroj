44af8818821746cfc4e79f5bff9f62ce8caf24c6
Edim Omerhodzic <omerhodzic.edim@outlook.com>
2023-04-18 21:29:08 +0200

    Base structure for react app

--- app/src/index.js
+++ app/src/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Layout from "./pages/layout";
+import Quiz from "./pages/quiz";
+import Forum from "./pages/forum";
+import NoPage from "./pages/nopage";
 
-// Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
-import "bootstrap/dist/js/bootstrap.bundle.min";
-
-import './index/css/index.css';
-
-import App from "./index/components/App";
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="forum" element={<Forum />} />
+          <Route path="quiz" element={<Quiz />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-      <App/>
-  </React.StrictMode>
-);
+root.render(<App />);
