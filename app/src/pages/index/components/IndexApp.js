0992108ee7e0c165114311ab34563a11e84dcabc
9ensar8 <ensarhorozovic@gmail.com>
2023-05-28 19:00:23 +0200

    update: Searchfield1 placed

--- app/src/pages/index/components/IndexApp.js
+++ app/src/pages/index/components/IndexApp.js
@@ -1,14 +1,16 @@
-import "../css/IndexApp.css"
+import "../css/IndexApp.css";
 import Button1 from "../../../shared_components/Button1";
 import LogoComponent from "../../../shared_components/LogoComponent";
+import SearchField1 from "../../../shared_components/Searchfield1";
 
-function IndexApp(){
-    return (
-        <>
-            <Button1 text={"Testiranje"} fontSize={"30px"} />
-            <LogoComponent />
-        </>
-    )
+function IndexApp() {
+  return (
+    <>
+      <Button1 text={"Testiranje"} fontSize={"30px"} />
+      <LogoComponent />
+      <SearchField1 />
+    </>
+  );
 }
 
 export default IndexApp;
