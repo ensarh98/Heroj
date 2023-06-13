6af150778ba32366219f32fe99fd3089d2c8ae26
9ensar8 <ensarhorozovic@gmail.com>
2023-06-13 19:38:54 +0200

    searchfield autocomplete words

--- app/src/shared_components/Searchfield1.js
+++ app/src/shared_components/Searchfield1.js
@@ -1,15 +1,30 @@
 import React, { useRef, useState } from "react";
 import "./Searchfield1.css";
 
-function App(props) {
+function Searchfield1(props) {
   const [searchText, setSearchText] = useState("");
   const [isPlaceholderVisible, setIsPlaceholderVisible] = useState(true);
   const inputRef = useRef(null);
 
-  function handleSearchText() {
-    console.log(searchText);
+  const [filteredData, setFilteredData] = useState([]);
+
+  const handleFilter = (e) => {
+    const searchWord = e.target.value;
+    const newFilter = props.data.filter((value) => {
+      return value.author.toLowerCase().includes(searchWord.toLowerCase());
+    });
+
+    if (searchWord === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
+
+  const handleSearchText = () => {
+    setFilteredData([]);
     setSearchText("");
-  }
+  };
 
   const handleInputFocus = () => {
     setIsPlaceholderVisible(false);
@@ -22,27 +37,48 @@ function App(props) {
 
   return (
     <>
-      <div className="wrapp-search-bar">
-        <input
-          className="search-bar"
-          type="search"
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-          placeholder={isPlaceholderVisible ? props.placeholder : ""}
-          onFocus={handleInputFocus}
-          onBlur={handleInputBlur}
-          ref={inputRef}
-        />
-        <button className="search-button" onClick={handleSearchText}>
-          <img
-            src="../images/search-icon.png"
-            className="search-bar-icon"
-            alt="search"
+      <div className="search-column">
+        <div className="wrapp-search-bar">
+          <input
+            className="search-bar"
+            type="search"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+              handleFilter(e);
+            }}
+            placeholder={isPlaceholderVisible ? props.placeholder : ""}
+            onFocus={handleInputFocus}
+            onBlur={handleInputBlur}
+            ref={inputRef}
           />
-        </button>
+          <button className="search-button" onClick={handleSearchText}>
+            <img
+              src="../images/search-icon.png"
+              className="search-bar-icon"
+              alt="search"
+            />
+          </button>
+        </div>
+        {filteredData.length != 0 && (
+          <div className="dataResult">
+            {filteredData.slice(0, 5).map((value, key) => {
+              return (
+                <a
+                  className="dataItem"
+                  onClick={handleSearchText}
+                  href={value.link}
+                  target="_blank"
+                >
+                  <p>{value.author}</p>
+                </a>
+              );
+            })}
+          </div>
+        )}
       </div>
     </>
   );
 }
 
-export default App;
+export default Searchfield1;
