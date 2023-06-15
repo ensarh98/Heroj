13fab1be7e0f07dcb89d87697551ad3358dc199c
Edim Omerhodzic <omerhodzic.edim@outlook.com>
2023-06-15 19:18:53 +0200

    add view_count for cases

--- heroj_api/template/models.py
+++ heroj_api/template/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class FirstAidCase(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
     title = models.CharField(max_length=255)
+    view_count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
