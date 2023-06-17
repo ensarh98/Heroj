8b5e379d50f7907b4b5ed125d02436c360e2c839
9ensar8 <ensarhorozovic@gmail.com>
2023-06-17 18:41:36 +0200

    designed login and register page

--- app/src/pages/login/Login.js
+++ app/src/pages/login/Login.js
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import Button1 from "../../shared_components/Button1";
 import Form from "../../shared_components/Form";
 import FormControl from "../../shared_components/FormControl";
@@ -6,28 +7,31 @@ import FormInput from "../../shared_components/FormInput";
 import FormLabel from "../../shared_components/FormLabel";
 import React from "react";
 import axios from "axios";
-import Cookies from 'universal-cookie';
+import Cookies from "universal-cookie";
 import { useNavigate } from "react-router-dom";
-
+import LogoComponent from "../../../src/shared_components/LogoComponent";
+import Sidebar from "../../shared_components/Sidebar";
+import ShowSidebar from "../../shared_components/ShowSidebarButton";
+import "./Login.css";
 
 export default function Login() {
-
   const navigate = useNavigate();
 
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
   const [remember, setRemember] = React.useState(false);
+  const sidebarRef = useRef(null);
 
   const cookies = new Cookies();
 
   const [errors, setErrors] = React.useState({
     username: {
       value: false,
-      msg: ''
+      msg: "",
     },
     password: {
       value: false,
-      msg: ''
+      msg: "",
     },
   });
 
@@ -36,71 +40,121 @@ export default function Login() {
       ...errors,
       [key]: {
         value: value,
-        msg: msg
-      }
-    })
-  }
+        msg: msg,
+      },
+    });
+  };
 
   const onSubmit = () => {
-    axios.post(`${process.env.REACT_APP_API}forum/login`, {
-      username,
-      password,
-      remember
-    }).then((res) => {
-      if (res.data.error) {
-        setErrorField(res.data.error.field, true, res.data.error.msg);
-      } else if (res.data.session_token) {
-        cookies.set('session_token', res.data['session_token'], { expires: new Date(res.data['expires']) });
-        navigate('/');
-      }
-    });
-  }
+    axios
+      .post(`${process.env.REACT_APP_API}forum/login`, {
+        username,
+        password,
+        remember,
+      })
+      .then((res) => {
+        if (res.data.error) {
+          setErrorField(res.data.error.field, true, res.data.error.msg);
+        } else if (res.data.session_token) {
+          cookies.set("session_token", res.data["session_token"], {
+            expires: new Date(res.data["expires"]),
+          });
+          navigate("/");
+        }
+      });
+  };
 
   const invalidUsername = (username) => {
     return !username.match("^[a-zA-Z0-9_]{5,15}$");
-  }
+  };
 
   const invalidPassword = (password) => {
     return !password.match("^[a-zA-Z0-9_]{6,15}$");
-  }
+  };
 
   const onChangeUsername = (username) => {
     setUsername(username);
     setErrorField(
-      'username',
+      "username",
       invalidUsername(username),
-      'Username must contain only alphanumeric values or _, at least 5 and max 15 characters.'
+      "Username must contain only alphanumeric values or _, at least 5 and max 15 characters."
     );
-  }
+  };
 
   const onChangePassword = (password) => {
     setPassword(password);
     setErrorField(
-      'password',
+      "password",
       invalidPassword(password),
-      'Password must contain only alphanumeric values or _, at least 6 and max 15 characters.'
+      "Password must contain only alphanumeric values or _, at least 6 and max 15 characters."
     );
-  }
+  };
+
+  const openNav = () => {
+    sidebarRef.current.style.left = "0";
+  };
+
+  const closeNav = () => {
+    sidebarRef.current.style.left = "-400px";
+  };
 
   return (
-    <div>
-      <Form>
-        <FormGroup>
-          <FormLabel for={"username"} text={"Korisničko ime"}/>
-          <FormInput name={"username"} type={"text"} onChange={e => onChangeUsername(e.target.value)}/>
-          <FormControl text={errors.username.msg} isInvalid={errors.username.value}/>
-        </FormGroup>
+    <>
+      <div className="reg-wrapper">
+        <div className="reg-left">
+          <Sidebar innerRef={sidebarRef} closeNav={closeNav} />
+          <div id="heading-left">
+            <ShowSidebar onClick={openNav} />
+          </div>
+        </div>
+        <div className="reg-right">
+          <div className="pic-warpper">
+            <div className="head-pic"></div>
+          </div>
+          <div className="header-text-log">LOGIN</div>
+          <div className="login-form">
+            <Form>
+              <FormGroup>
+                <FormLabel for={"username"} text={"Korisničko ime"} />
+                <FormInput
+                  name={"username"}
+                  type={"text"}
+                  onChange={(e) => onChangeUsername(e.target.value)}
+                />
+                <FormControl
+                  text={errors.username.msg}
+                  isInvalid={errors.username.value}
+                />
+              </FormGroup>
 
-        <FormGroup>
-          <FormLabel for={"password"} text={"Šifra"}/>
-          <FormInput name={"password"} type={"password"} onChange={e => onChangePassword(e.target.value)}/>
-          <FormControl text={errors.password.msg} isInvalid={errors.password.value}/>
-        </FormGroup>
+              <FormGroup paddingBottom={"30px"}>
+                <FormLabel for={"password"} text={"Šifra"} />
+                <FormInput
+                  name={"password"}
+                  type={"password"}
+                  onChange={(e) => onChangePassword(e.target.value)}
+                />
+                <FormControl
+                  text={errors.password.msg}
+                  isInvalid={errors.password.value}
+                />
+              </FormGroup>
 
-        <FormGroup>
-          <Button1 text={"Prijavi se"} fontSize={"20px"} onClick={onSubmit}></Button1>
-        </FormGroup>
-      </Form>
-    </div>
+              <FormGroup>
+                <Button1
+                  text={"Prijavi se"}
+                  fontSize={"20px"}
+                  onClick={onSubmit}
+                ></Button1>
+              </FormGroup>
+            </Form>
+          </div>
+
+          <div className="logo-component-log">
+            <LogoComponent />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
--- app/src/pages/register/Register.js
+++ app/src/pages/register/Register.js
@@ -1,37 +1,43 @@
+import { useRef } from "react";
 import Button1 from "../../shared_components/Button1";
 import Form from "../../shared_components/Form";
 import FormControl from "../../shared_components/FormControl";
 import FormGroup from "../../shared_components/FormGroup";
 import FormInput from "../../shared_components/FormInput";
 import FormLabel from "../../shared_components/FormLabel";
+import "./register.css";
+import LogoComponent from "../../../src/shared_components/LogoComponent";
+import Sidebar from "../../shared_components/Sidebar";
+import ShowSidebar from "../../shared_components/ShowSidebarButton";
 
 import React from "react";
 import axios from "axios";
 
 export default function Register() {
-  const [email, setEmail] = React.useState('');
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [confirmPassword, setConfirmPassword] = React.useState('');
+  const [email, setEmail] = React.useState("");
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [confirmPassword, setConfirmPassword] = React.useState("");
+  const sidebarRef = useRef(null);
 
   const [success, setSuccess] = React.useState(false);
 
   const [errors, setErrors] = React.useState({
     email: {
       value: false,
-      msg: ''
+      msg: "",
     },
     username: {
       value: false,
-      msg: ''
+      msg: "",
     },
     password: {
       value: false,
-      msg: ''
+      msg: "",
     },
     confirmPassword: {
       value: false,
-      msg: ''
+      msg: "",
     },
   });
 
@@ -40,107 +46,170 @@ export default function Register() {
       ...errors,
       [key]: {
         value: value,
-        msg: msg
-      }
-    })
-  }
+        msg: msg,
+      },
+    });
+  };
 
   const onSubmit = () => {
-    axios.post(`${process.env.REACT_APP_API}forum/register`, {
-      email,
-      username,
-      password
-    }).then((res) => {
-      if (res.data.error) {
-        setErrorField(res.data.error.field, true, res.data.error.msg);
-      } else if (res.data.success) {
-        setSuccess(true);
-      }
-    });
-  }
+    axios
+      .post(`${process.env.REACT_APP_API}forum/register`, {
+        email,
+        username,
+        password,
+      })
+      .then((res) => {
+        if (res.data.error) {
+          setErrorField(res.data.error.field, true, res.data.error.msg);
+        } else if (res.data.success) {
+          setSuccess(true);
+        }
+      });
+  };
 
   const invalidUsername = (username) => {
     return !username.match("^[a-zA-Z0-9_]{5,15}$");
-  }
+  };
 
   const invalidEmail = (email) => {
-    return !email.match("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$");
-  }
+    return !email.match(
+      "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$"
+    );
+  };
 
   const invalidPassword = (password) => {
     return !password.match("^[a-zA-Z0-9_]{6,15}$");
-  }
+  };
 
   const invalidConfirmPassword = (confirmPassword) => {
     return confirmPassword !== password;
-  }
+  };
 
   const onChangeUsername = (username) => {
     setUsername(username);
     setErrorField(
-      'username',
+      "username",
       invalidUsername(username),
-      'Username must contain only alphanumeric values or _, at least 5 and max 15 characters.'
+      "Username must contain only alphanumeric values or _, at least 5 and max 15 characters."
     );
-  }
+  };
 
   const onChangeEmail = (email) => {
     setEmail(email);
-    setErrorField('email', invalidEmail(email), 'Email format is not valid.');
-  }
+    setErrorField("email", invalidEmail(email), "Email format is not valid.");
+  };
 
   const onChangePassword = (password) => {
     setPassword(password);
     setErrorField(
-      'password',
+      "password",
       invalidPassword(password),
-      'Password must contain only alphanumeric values or _, at least 6 and max 15 characters.'
+      "Password must contain only alphanumeric values or _, at least 6 and max 15 characters."
     );
-  }
+  };
 
   const onChangeConfirmPassword = (confirmPassword) => {
     setConfirmPassword(confirmPassword);
     setErrorField(
-      'confirmPassword',
+      "confirmPassword",
       invalidConfirmPassword(confirmPassword),
-      'Passwords must match!'
+      "Passwords must match!"
     );
-  }
+  };
+
+  const openNav = () => {
+    sidebarRef.current.style.left = "0";
+  };
+
+  const closeNav = () => {
+    sidebarRef.current.style.left = "-400px";
+  };
 
   return (
-    <div>
-      <Form>
-        <FormGroup>
-          <FormLabel for={"username"} text={"Korisničko ime"}/>
-          <FormInput name={"username"} type={"text"} onChange={e => onChangeUsername(e.target.value)}/>
-          <FormControl text={errors.username.msg} isInvalid={errors.username.value}/>
-        </FormGroup>
+    <>
+      <div className="reg-wrapper">
+        <div className="reg-left">
+          <Sidebar innerRef={sidebarRef} closeNav={closeNav} />
+          <div id="heading-left">
+            <ShowSidebar onClick={openNav} />
+          </div>
+        </div>
+        <div className="reg-right">
+          <div className="pic-warpper">
+            <div className="head-pic"></div>
+          </div>
+          <div className="header-text-reg">REGISTRACIJA</div>
+          <div className="register-form">
+            <Form>
+              <FormGroup>
+                <FormLabel for={"username"} text={"Korisničko ime"} />
+                <FormInput
+                  name={"username"}
+                  type={"text"}
+                  onChange={(e) => onChangeUsername(e.target.value)}
+                />
+                <FormControl
+                  text={errors.username.msg}
+                  isInvalid={errors.username.value}
+                />
+              </FormGroup>
 
-        <FormGroup>
-          <FormLabel for={"email"} text={"Email"}/>
-          <FormInput name={"email"} type={"email"} onChange={e => onChangeEmail(e.target.value)}/>
-          <FormControl text={errors.email.msg} isInvalid={errors.email.value}/>
-        </FormGroup>
+              <FormGroup>
+                <FormLabel for={"email"} text={"Email"} />
+                <FormInput
+                  name={"email"}
+                  type={"email"}
+                  onChange={(e) => onChangeEmail(e.target.value)}
+                />
+                <FormControl
+                  text={errors.email.msg}
+                  isInvalid={errors.email.value}
+                />
+              </FormGroup>
 
-        <FormGroup>
-          <FormLabel for={"password"} text={"Šifra"}/>
-          <FormInput name={"password"} type={"password"} onChange={e => onChangePassword(e.target.value)}/>
-          <FormControl text={errors.password.msg} isInvalid={errors.password.value}/>
-        </FormGroup>
+              <FormGroup>
+                <FormLabel for={"password"} text={"Šifra"} />
+                <FormInput
+                  name={"password"}
+                  type={"password"}
+                  onChange={(e) => onChangePassword(e.target.value)}
+                />
+                <FormControl
+                  text={errors.password.msg}
+                  isInvalid={errors.password.value}
+                />
+              </FormGroup>
 
-        <FormGroup paddingBottom={"50px"}>
-          <FormLabel for={"confirmPassword"} text={"Ponovi Šifru"}/>
-          <FormInput name={"confirmPassword"} type={"password"} onChange={e => onChangeConfirmPassword(e.target.value)}/>
-          <FormControl text={errors.confirmPassword.msg} isInvalid={errors.confirmPassword.value}/>
-        </FormGroup>
+              <FormGroup paddingBottom={"30px"}>
+                <FormLabel for={"confirmPassword"} text={"Ponovi Šifru"} />
+                <FormInput
+                  name={"confirmPassword"}
+                  type={"password"}
+                  onChange={(e) => onChangeConfirmPassword(e.target.value)}
+                />
+                <FormControl
+                  text={errors.confirmPassword.msg}
+                  isInvalid={errors.confirmPassword.value}
+                />
+              </FormGroup>
 
-        <FormGroup>
-          <Button1 text={"Registriraj se"} fontSize={"20px"} onClick={onSubmit}></Button1>
-        </FormGroup>
-      </Form>
-      { success && 
-        "Uspješno ste se registrirali! Provjerite email za aktivaciju vašeg računa." 
-      }
-    </div>
+              <FormGroup>
+                <Button1
+                  text={"Registriraj se"}
+                  fontSize={"20px"}
+                  onClick={onSubmit}
+                ></Button1>
+              </FormGroup>
+            </Form>
+            {success &&
+              "Uspješno ste se registrirali! Provjerite email za aktivaciju vašeg računa."}
+          </div>
+
+          <div className="logo-component-reg">
+            <LogoComponent />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
