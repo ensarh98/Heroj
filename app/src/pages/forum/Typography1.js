5837b25368081e589bd9c17d881a5d2575362216
Edim Omerhodzic <omerhodzic.edim@outlook.com>
2023-06-18 00:49:55 +0200

    forum v0.4

---
+++ app/src/pages/forum/Typography1.js
@@ -0,0 +1,9 @@
+import "./Typography1.css"
+
+export default function Typography1(props) {
+  return (
+    <div className="Typography1">
+      {props.children}
+    </div>
+  )
+}
--- app/src/pages/forum/index.js
+++ app/src/pages/forum/index.js
@@ -1,13 +1,36 @@
 import axios from "axios";
 import './index.css'
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import RelevantCard from "./RelevantCard";
 import ForumTopicCard from "./ForumTopicCard";
+import Typography1 from "./Typography1";
+import ShowSidebar from "../../shared_components/ShowSidebarButton";
+import Sidebar from "../../shared_components/Sidebar";
+import Button1 from "../../shared_components/Button1";
 
 export default function Forum() {
+  const weekday = ["Nedjelja","Ponedeljak","Utorak","Srijeda","Četvrtak","Petak","Subota"];
 
   const [forumData, setForumData] = useState([])
   
+  const sidebarRef = useRef(null);
+
+  const openNav = () => {
+    sidebarRef.current.style.left = "0";
+  };
+
+  const closeNav = () => {
+    sidebarRef.current.style.left = "-400px";
+  };
+
+  const handleClickSignUp = () => {
+    window.location.href = "http://localhost:3000/register/";
+  };
+
+  const handleClickLogIn = () => {
+    window.location.href = "http://localhost:3000/login/";
+  };
+
   React.useEffect(() => {
     axios.get("http://localhost:8000/forum/forums/general")
       .then(res => setForumData(res.data.topics));
@@ -15,12 +38,42 @@ export default function Forum() {
 
   return (
     <>
-      <RelevantCard name={"Teme"}>
-        { forumData && forumData.map((value) => (
-          <ForumTopicCard dayOfTheWeek={"Utorak"} date={value.date_modified} repliesNumber={"22"} topicText={value.title} username={value.created_by}/>
-          ))
-        }
-      </RelevantCard>
+      <Sidebar innerRef={sidebarRef} closeNav={closeNav} />
+      <div className="row1">
+        <div>
+          <ShowSidebar onClick={openNav} />
+        </div>
+        <div className="heading-container">
+          <Typography1>
+            FORUM
+          </Typography1>
+        </div>
+        <div className="login-container">
+          <Button1
+            text={"Prijava"}
+            fontSize={"25px"}
+            onClick={handleClickLogIn}
+          />
+          <Button1
+            text={"Registracija"}
+            fontSize={"25px"}
+            onClick={handleClickSignUp}
+          />
+        </div>
+      </div>
+      <div className="forum-container">
+        <RelevantCard name={"Teme"}>
+          { forumData && forumData.map((value) => (
+            <ForumTopicCard dayOfTheWeek={weekday[new Date(value.date_created).getDay()]} 
+              date={value.date_created}
+              repliesNumber={"22"} 
+              topicText={value.title} 
+              username={value.created_by}
+            />
+            ))
+          }
+        </RelevantCard>
+      </div>
     </>
   );
 
