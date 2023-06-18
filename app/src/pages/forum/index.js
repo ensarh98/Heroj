d8d1274149794fa1652c040e4ab58e0d422bc380
Edim Omerhodzic <omerhodzic.edim@outlook.com>
2023-06-18 15:33:35 +0200

    add logged in to forum page

--- app/src/pages/forum/index.js
+++ app/src/pages/forum/index.js
@@ -7,6 +7,8 @@ import Typography1 from "./Typography1";
 import ShowSidebar from "../../shared_components/ShowSidebarButton";
 import Sidebar from "../../shared_components/Sidebar";
 import Button1 from "../../shared_components/Button1";
+import Cookies from "universal-cookie";
+import LogedIn from "../../shared_components/LogedIn";
 
 export default function Forum() {
   const weekday = ["Nedjelja","Ponedeljak","Utorak","Srijeda","Četvrtak","Petak","Subota"];
@@ -14,6 +16,10 @@ export default function Forum() {
   const [forumData, setForumData] = useState([])
   
   const sidebarRef = useRef(null);
+
+  const [username, setUsername] = useState("");
+
+  const cookies = new Cookies();
 
   const openNav = () => {
     sidebarRef.current.style.left = "0";
@@ -34,6 +40,9 @@ export default function Forum() {
   React.useEffect(() => {
     axios.get("http://localhost:8000/forum/forums/general")
       .then(res => setForumData(res.data.topics));
+
+    if (cookies.get("session_token")) axios.get(`http://localhost:8000/forum/user/${cookies.get("session_token")}`)
+      .then((res) => setUsername(res.data.username));
   }, []);
 
   return (
@@ -49,16 +58,23 @@ export default function Forum() {
           </Typography1>
         </div>
         <div className="login-container">
-          <Button1
-            text={"Prijava"}
-            fontSize={"25px"}
-            onClick={handleClickLogIn}
-          />
-          <Button1
-            text={"Registracija"}
-            fontSize={"25px"}
-            onClick={handleClickSignUp}
-          />
+          { !cookies.get("session_token") ?
+              <>
+                <Button1
+                  text={"Prijava"}
+                  fontSize={"25px"}
+                  onClick={handleClickLogIn}
+                />
+                <Button1
+                  text={"Registracija"}
+                  fontSize={"25px"}
+                  onClick={handleClickSignUp}
+                />
+              </> :
+              <>
+                <LogedIn username={username} />
+              </>
+            }
         </div>
       </div>
       <div className="forum-container">
@@ -77,104 +93,4 @@ export default function Forum() {
       </div>
     </>
   );
-
-  // return (
-  //     <>
-  //   <RelevantCard name={"Popularno"}>
-  //       <RelevantCardRow text={"Popularno 1"} link={"nekiUrl"}/>
-  //       <RelevantCardRow text={"Popularno 2"} link={"nekiUrl"}/>
-  //       <RelevantCardRow text={"Popularno 3fgkjdshgkjfdshgkjhfdkjghkjfdhg"} link={"nekiUrl"}/>
-  //       <RelevantCardRow text={"Popularno 3fgkjdshgkjfdshgkjhfdkjghkjfdhg"} link={"nekiUrl"}/>
-  //       <RelevantCardRow text={"Popularno 3fgkjdshgkjfdshgkjhfdkjghkjfdhg"} link={"nekiUrl"}/>
-  //     </RelevantCard>
-
-  //   <ForumTopicCard dayOfTheWeek={"Utorak"} date={"22/03/2023"} repliesNumber={"22"} topicText={"Topic "} username={"HarunHadzic22"}/>
-  //     <ForumTopicCard dayOfTheWeek={"Utorak"} date={"22/03/2023"} repliesNumber={"22"} topicText={"TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST"} username={"HarunHadzic22"}/>
-
-  //         <ForumRepliesCard time={"23:08:22"} date={"13/06/2022"} username={"harun.hadzic"} text={"Primjerak testa"} replyTo={"Tema neka"} replyNumber={"1"}/>
-
-  //         <LogedIn username={"HarunHadzic22"}/>
-  //     </>
-    /*
-  <>
-    <Container>
-      <Row>
-        <Col>
-          <ForumNavbar></ForumNavbar>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Breadcrumb>
-            <Breadcrumb.Item href="#" active>Forum</Breadcrumb.Item>
-          </Breadcrumb>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={9}>
-          <ForumCard title={<h4>Forums</h4>}>
-            {forums.map((forum) => (
-              <Link to={`/forum/${forum.pk}`}>
-                {forum.fields.title}
-              </Link>
-            ))}
-          </ForumCard>
-          {forums.map((forum) => (
-            <ForumCard title={<h4>{forum.fields.title}</h4>}>
-              <Link to={`#`}>
-                Post 1
-              </Link>
-              <Link to={`#`}>
-                Post 2
-              </Link>
-              <Link to={`#`}>
-                Post 3
-              </Link>
-              <Link to={`#`}>
-                Post 4
-              </Link>
-            </ForumCard>
-          ))}
-        </Col>
-        <Col>
-          <ForumSideCard title={"Recent posts"}>
-            <Link to={`#`}>
-              Post 1
-            </Link>
-            <Link to={`#`}>
-              Post 2
-            </Link>
-            <Link to={`#`}>
-              Post 3
-            </Link>
-            <Link to={`#`}>
-              Post 4
-            </Link>
-            <Link to={`#`}>
-              Post 4
-            </Link>
-          </ForumSideCard>
-          <ForumSideCard title={"Top posts"}>
-            <Link to={`#`}>
-              Post 1
-            </Link>
-            <Link to={`#`}>
-              Post 2
-            </Link>
-            <Link to={`#`}>
-              Post 3
-            </Link>
-            <Link to={`#`}>
-              Post 4
-            </Link>
-            <Link to={`#`}>
-              Post 4
-            </Link>
-          </ForumSideCard>
-        </Col>
-      </Row>
-    </Container>
-  </>
-
-    ); */
 }
